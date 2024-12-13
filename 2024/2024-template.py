87af8760f45d2909f6145032f67e6dd659e16268
Jon Kracht <jonkracht@gmail.com>
2024-12-13 14:06:38 -0500

    Update template

--- 2024/2024-template.py
+++ 2024/2024-template.py
@@ -5,3 +5,8 @@ infile = sys.argv[1] if len(sys.argv)>=2 else './data/3.in'
 data = open(infile).read().strip()
 
 print(data)
+
+
+
+#print(f"{'Solution to Part 1:':<20} {p1}")
+#print(f"{'Solution to Part 2:':<20} {p2}")
