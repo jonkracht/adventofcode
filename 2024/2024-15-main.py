0061384ce47cc7641700fbdcbd94fbdb6f2523b4
Jon Kracht <jonkracht@gmail.com>
2025-01-16 22:20:10 -0500

    Day 15 solved

--- 2024/2024-15-main.py
+++ 2024/2024-15-main.py
@@ -9,42 +9,38 @@ warehouse, moves = raw.split("\n\n")
 moves = [m for m in moves if m != "\n"]
 warehouse = [[ww for ww in list(w)] for w in warehouse.split('\n')]
 
-
-# Find starting location
+# Find starting robot location
 for i, w in enumerate(warehouse):
     for j, ww in enumerate(w):
         if ww == "@":
             start = (i, j)
 
 
-def make_move(move, wh, loc):
-    """"""
+def make_move_p1(move, wh, loc):
+    """Update warehouse based on next move"""
     
     # Convert arrow character into vector
     vectors = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
-    
     dir = vectors[move]
 
     next_loc = (loc[0] + dir[0], loc[1] + dir[1])
     char = wh[next_loc[0]][next_loc[1]]
 
-    if char == ".":  # open space - switch robot and open space
+    if char == ".":  # open space:  move robot there and set previous location to '.'
         wh[next_loc[0]][next_loc[1]] = "@"
         wh[loc[0]][loc[1]] = "."
 
         return wh, next_loc
 
-    elif char == "O":
-        
-        boxes = [next_loc]
+    elif char == "O":  # box is encountered
+        boxes = [next_loc]  # list of boxes to be moved in appropriate direction
 
         while True:
+            # Compute candidate next location/character
             next_loc = (boxes[-1][0] + dir[0], boxes[-1][1] + dir[1])
-
             next_char = wh[next_loc[0]][next_loc[1]]
 
-            if next_char == ".":
-                # Update warehouse
+            if next_char == ".":  # Empty space at end of boxes - move them and robot
                 wh[loc[0]][loc[1]] = "."
                 wh[next_loc[0]][next_loc[1]] = "O"
                 
@@ -57,13 +53,10 @@ def make_move(move, wh, loc):
                 # Add box to list of things to be moved
                 boxes.append((next_loc))
 
-            elif next_char == "#":
-                # If a wall is eventually encountered, do nothing
+            elif next_char == "#":  # wall encountered - do nothing
                 return wh, loc
 
-
-
-    elif char == "#":  # wall encountered - do nothing and return 
+    elif char == "#":  # wall encountered - do nothing
         return wh, loc
 
     else:
@@ -83,12 +76,12 @@ def compute_gps(wh: list, char: str):
 
     return gps
 
+
 warehouse1 = copy.deepcopy(warehouse)
 robot_loc = start
 
-
 for m in moves:
-    warehouse1, robot_loc = make_move(m, warehouse1, robot_loc)
+    warehouse1, robot_loc = make_move_p1(m, warehouse1, robot_loc)
 
 gps1 = compute_gps(warehouse1, "O")
 
@@ -116,16 +109,170 @@ def widen_warehouse(warehose):
     return n
 
 
-warehouse2 = widen_warehouse(warehouse)
-#print('\n'.join([''.join(w) for w in warehouse2]))  # print warehouse
+def make_move_p2(move, wh, loc):
+    """Update warehouse for widened warehouse in Part 2."""
+    
+    # Convert arrow character into vector
+    vectors = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
+    dir = vectors[move]
 
-robot_loc = start
+    next_loc = (loc[0] + dir[0], loc[1] + dir[1])
+    next_char = wh[next_loc[0]][next_loc[1]]
 
-for m in moves:
-    #warehouse2 = make_move_2()
-    pass
+    print(f"Robot at {loc}.  Move direction is {move} ({dir}) where character is '{next_char}'.")
+
+    if next_char == ".":
+        print("Empty space.  Moving into it and updating warehouse.")
+        #print(loc)
+        #print(next_loc)
+        wh[next_loc[0]][next_loc[1]] = "@"
+        wh[loc[0]][loc[1]] = '.'
+        return wh, next_loc
+
+    if next_char == "#":
+        print("Hit a wall.  Doing nothing.")
+        return wh, loc
+
+    boxes_to_move = set()
+    
+    # Hit one or more boxes.  Handle horizontal and vertical moves separately.
+    if dir[0] == 0:  # horizontal move - only lines of boxes move
+        if next_char == "[":
+            boxes_to_move.add(next_loc)
+        elif next_char == "]":
+            boxes_to_move.add((next_loc[0], next_loc[1] - 1))
+            next_loc = (next_loc[0], next_loc[1] - 1)
+        else:
+            print(f"Unexpected character:  {next_char}")
+            return
+       
+        print(f"Initialized boxes_to_move:  {boxes_to_move}")
+        while True:
+
+            next_loc = (next_loc[0] + dir[0], next_loc[1] + 1 * dir[1])
+            next_char = wh[next_loc[0]][next_loc[1]]
+
+            if next_char == "#":
+                print("Hit a wall. Do nothing.")
+                return wh, loc
+
+            elif next_char == "[":
+                boxes_to_move.add(next_loc)
+
+            elif next_char == ".":
+                print(f"Moving boxes:  {boxes_to_move}")
+                for box in boxes_to_move:
+                    wh[box[0]][box[1] + dir[1]] = "[" 
+                    wh[box[0]][box[1] + dir[1] + 1]  = "]" 
+
+                wh[loc[0] + dir[0]][loc[1] + dir[1]] = "@"
+                wh[loc[0]][loc[1]] = "."
+
+                return wh, (loc[0] + dir[0], loc[1] + dir[1])
 
 
-gps2 = compute_gps(warehouse2, '[')
+    # Vertical moves - clusters of blocks may need to be moved. 
+    elif dir[1] == 0:
+        #print("Moving vertically")
+        if next_char == "[":
+            boxes_to_move.add(next_loc)
+            const = 1
+        elif next_char == "]":
+            boxes_to_move.add((next_loc[0], next_loc[1] - 1))
+            const = -1
+        else:
+            print(f"Unexpected character: {next_char}")
+            return
 
-print(f"{'Solution to Part 2:':<20} {gps2}")
+        print(f"Initialized boxes_to_move:  {boxes_to_move}")
+
+        all_boxes = set()
+        
+        while True:
+            #print(f"All boxes:  {boxes_to_move}")
+            canMove = True
+            next_boxes = set()
+            
+            for box in boxes_to_move:
+                #print(box)
+                
+                next_boxes.add(box)
+                
+                # For each box, check two points it touches
+                check1 = (box[0] + dir[0], box[1] + dir[1])
+                check2 = (box[0] + dir[0], box[1] + dir[1] + 1)
+
+                char1 = wh[check1[0]][check1[1]]
+                char2 = wh[check2[0]][check2[1]]
+
+                #print(f"\nBox {box}: check 1:  {check1}, check 2: {check2}")
+                #print(f"\nchar 1:  {char1}, char 2: {char2}")
+
+                if (char1 == "#") or (char2 == "#"):
+                    print("Hit a wall.  Stopping")
+                    return wh, loc 
+                if char1 == "[":
+                    if check1 not in all_boxes:
+                        canMove = False
+                    next_boxes.add(check1)
+                if char1 == "]":
+                    temp = (check1[0], check1[1] - 1)
+                    if temp not in all_boxes:
+                        canMove = False
+                    next_boxes.add(temp)
+                if char2 == "[":
+                    if check2 not in all_boxes:
+                        canMove = False
+                    next_boxes.add(check2)
+                    
+            boxes_to_move = next_boxes
+
+            for n in next_boxes:
+                all_boxes.add(n)
+           
+            # If no obstacle is found:
+            if canMove:
+                #print(f"Moving boxes: {all_boxes}") 
+                # Remove boxes and later add them in at their new locations 
+                for n in all_boxes:
+                    wh[n[0]][n[1]] = "." 
+                    wh[n[0]][n[1] + 1] = "."
+
+                wh[loc[0]][loc[1]] = "." 
+                
+                for n in all_boxes:
+                    wh[n[0] + dir[0]][n[1]] = "[" 
+                    wh[n[0] + dir[0]][n[1] + 1] = "]"
+
+                     
+                wh[loc[0] + dir[0]][loc[1] + dir[1]] = "@" 
+                    
+
+                return wh, (loc[0] + dir[0], loc[1] + dir[1])
+                
+
+
+        return wh, loc
+
+
+warehouse2 = widen_warehouse(copy.deepcopy(warehouse))
+print("\nInitial configuration:")
+print('\n'.join([''.join(w) for w in warehouse2]))  # print warehouse
+
+# Find starting robot location
+for i, w in enumerate(warehouse2):
+    for j, ww in enumerate(w):
+        if ww == "@":
+            start2 = (i, j)
+
+robot_loc = start2
+
+for i, m in enumerate(moves[:]):
+    print(f"\nMove {i}:")
+    warehouse2, robot_loc = make_move_p2(moves[i], copy.deepcopy(warehouse2), robot_loc)
+    print('\n'.join([''.join(w) for w in warehouse2]))  # print warehouse
+    #input("Press enter for next iteration")
+
+gps2 = compute_gps(warehouse2.copy(), '[')
+print(f"{'\nSolution to Part 2:':<20} {gps2}")
+
