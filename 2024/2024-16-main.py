d8b62432ebd98173a70d3559521b2b61466949a0
Jon Kracht <jonkracht@gmail.com>
2025-01-31 19:55:30 -0500

    Day 16 solved prettily

--- 2024/2024-16-main.py
+++ 2024/2024-16-main.py
@@ -1,147 +1,137 @@
 import sys
-sys.setrecursionlimit(10**6)
-infile = sys.argv[1] if len(sys.argv) >= 2 else './data/16.in'
 
-raw = open(infile).read().strip()
-data = raw.split("\n")
+infile = sys.argv[1] if len(sys.argv)>=2 else './data/16.in'
+data = open(infile).read().strip().split("\n")
+
+starting_direction = (0, 1)  # problem states initial direction faces east
+
+vertices = set()
+for i, row in enumerate(data):
+    for ii, val in enumerate(row):
+        if val != "#":
+            vertices.add((i, ii))
+            if val == "S":
+                starting_pt = (i, ii)
+            elif val == "E":
+                finish_pt = (i, ii)
+
+motions = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)}  # right, down, left, up
+
+starting_dir_ordinal = [k for k, v in motions.items() if v == starting_direction][0]
 
 
-# Implement Dijkstra's algorithm using Wikipedia pseudocode
-def Dijkstra(vertices, start, starting_dir):
-    """Implement Dijkstra's algorithm with turning penalty."""
+def dijkstra(vertices, start, start_dir):
+    """Use Dijkstra's algorithm to find the shortest distance/cost to each point."""
 
-    # Define a dictionary for direction
-    motions = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)}
+    '''
+    Graph is a dictionary with keys equal to state (position, orientation) with
+    values of lists containing has_been_processed Boolean, score and list of previous point(s).
+    '''
 
-    # Find key of "starting_dir"
-    result = [key for key, val in motions.items() if val == starting_dir]
+    graph = {(start[0], start[1], start_dir): [False, 0, []]}
 
-    graph = {}
-    graph[(start[0], start[1], result[0])] = [False, 0, []]
+    # Iterate until all unique states have been processed
+    while sum([v[0] == False for k, v in graph.items()]) > 0:
 
-    while sum([g[0] == False for g in graph.values()]):
-
-        # Find vertex with lowest score
+        # Find known state with the lowest score:
         min_score = 1e8
-        for k, v in graph.items():
-            if v[0] == False and v[1] < min_score:
-                key, min_score = k, v[1]
+        for key, val in graph.items():
+            if val[0] == False and val[1] < min_score:
+                min_score, min_key = val[1], key
 
-        current_state = key
-        graph[key][0] = True
+        print(min_score)  # display a measure of progress
 
-        # Attempt to move straight
-        next_loc = (current_state[0] + motions[current_state[2]][0], current_state[1] + motions[current_state[2]][1])
-        score = graph[key][1] + 1
+        graph[min_key][0] = True  # mark state as processed
 
-        if next_loc in vertices:
-            next_state = (next_loc[0], next_loc[1], current_state[2])
-            if next_state in graph.keys():
-                if score < graph[next_state][1]:
-                    graph[next_state] = [False, score, [current_state]]
-                elif score == graph[next_state][1]:
-                    graph[next_state][2].append(current_state)
+        i, j, direction = min_key
+        _, score, _ = graph[min_key]
+
+        states_to_add = {}
+
+        # Three actions possible:
+        # 1.)  Proceed straight (i.e. in current direction)
+        ii, jj = i + motions[direction][0], j + motions[direction][1]
+
+        if (ii, jj) in vertices:  # make sure candidate point is indeed a vertex
+            states_to_add[(ii, jj, direction)] = [False, score + 1, [(i, j, direction)]]
+
+        # 2.)  Turn clockwise
+        states_to_add[(i, j, (direction + 1) % 4)] = [False, score + 1000, [(i, j, direction)]]
+
+        # 3.)  Turn counter-clockwise
+        states_to_add[(i, j, (direction + 3) % 4)] = [False, score + 1000, [(i, j, direction)]]
+
+        # Check if new candidate states already exist in graph:
+        for k, v in states_to_add.items():
+            if k not in graph.keys():  # new state reached
+                graph[k] = v
+
             else:
-                graph[next_state] = [False, score, [current_state]]
+                # Better way to reach state
+                if v[1] < graph[k][1]:
+                    graph[k][1], graph[k][2] = v[1], v[2]
 
-        # Clockwise
-        score = graph[key][1] + 1000
-        next_state = (current_state[0], current_state[1], (current_state[2] + 1) % 4)
-        if next_state in graph.keys():
-            if score < graph[next_state][1]:
-                graph[next_state][1] = score
-            elif score == graph[next_state][1]:
-                graph[next_state][2].append(current_state)
-        else:
-            graph[next_state] = [False, score, [current_state]]
-
-        # Counter-clockwise
-        next_state = (current_state[0], current_state[1], (current_state[2] + 3) % 4)
-        if next_state in graph.keys():
-            if score < graph[next_state][1]:
-                graph[next_state][1] = score
-            elif score == graph[next_state][1]:
-                graph[next_state][2].append(current_state)
-        else:
-            graph[next_state] = [False, graph[key][1] + 1000, [current_state]]
+                # Equivalent way to reach state
+                elif v[1] == graph[k][1]:
+                    graph[k][2].append(v[2][0])
 
     return graph
 
 
+# Create graph
+graph = dijkstra(vertices, starting_pt, starting_dir_ordinal)
+
+# Find state(s) that reaches "finish" with best score
+best_score, best_finish = 1e8, []
+for k, v in graph.items():
+    i, j, _ = k
+    if (i, j) == finish_pt:
+        if v[1] < best_score:
+            best_score = v[1]
+            best_finish = [(k, v)]
+        elif v[1] == best_score:
+            best_finish.append((k, v))
+
+print(f"\n{'Solution to Part 1:':<20} {best_score}")
+
+print(f"({len(best_finish)} unique route(s) achieve this score.)")
 
 
-# Parse data in list of vertices, start and finish
-vertices = []
-for i, row in enumerate(data):
-    for ii, val in enumerate(row):
-        if val != "#":
-            vertices.append((i, ii))
-            if val == "S":
-                start = (i, ii)
-            elif val == "E":
-                finish = (i, ii)
+'''
+Part 2:  Find number of tiles in map that one or more best routes pass through
 
-starting_direction = (0, 1)  # problem states that begin facing east
+Strategy:  
+Starting best finishing state found in Part 1.
+Trace backwards through graph using previous state.
+If multiple previous states exist, note them and process when finished current route.
+'''
 
-print(f"Input file:  '{infile}'")
-print(f"Dimensions:  ({len(data)}, {len(data[0])})")
-print(f"Start {start}, finish {finish}")
-
-result = Dijkstra(vertices, start, starting_direction)
-
-# Find state(s) whose location is "finish" with lowest scores
-best_score = 1e8
-end_states = []
-
-for k, v in result.items():
-    row, col, dir = k
-    _, score, previous = v
-
-    if (row, col) == finish:
-        if score < best_score:
-            best_score = score
-            end_states.append(((row, col, dir), score, previous))
-
-print(f"\n{len(end_states)} unique end states:\n{end_states}")
-
-
-print(f"\n{'Solution to Part 1:':<20} {end_states[0][1]}")
-#print(best_keys)
-
-# Part 2:  Find number of tiles that a "best" path travels through
-unique_tiles = set()
-branch_count = 0
-for e in end_states:
-    state, score, previous = e
-    route = [state, previous[0]]
-    branches = []
-    branch_count += 1
+tiles_visited = set()
+for b in best_finish:
+    state = b[0]
+    branches = set()
 
     while True:
-        # Find next state:
-        #print(branches)
-        _, _, pp = result[route[-1]]
+        i, j, direction = state
+        tiles_visited.add((i, j))
 
-        if pp == []:
+        _, score, previous = graph[state]
+
+        if not previous:  # when "start" vertex is reached
             if len(branches) == 0:
                 break
             else:
-                pp = [branches.pop(0)];
+                state = branches.pop()
+                #print(f"End of route reached.  Starting again at branching state {state}")
 
+        # If one previous state exists, make it next state; if multiple, add extras to "branches" and process later
+        if len(previous) == 1:
+            state = previous[0]
+        else:
+            for i, p in enumerate(previous):
+                if i == 0:
+                    state = p
+                else:
+                    branches.add(p)
 
-        for i, ppp in enumerate(pp):
-            if i == 0:
-                route.append(ppp)
-            else:
-                branches.append(ppp)
-                branch_count += 1
-
-
-    for r in route:
-        x, y, dir = r
-        unique_tiles.add((x, y))
-
-p2 = len(unique_tiles)
-print(f"\n{'Solution to Part 2:':<20} {p2}")
-
-print(f"{branch_count} unique routes exist that produce this score")
+print(f"\n{'Solution to Part 2:':<20} {len(tiles_visited)}")
