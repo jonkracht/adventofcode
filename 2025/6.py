1216dfaae4fd10713ca3885683168ebfce09207d
Jon Kracht <jonkracht@gmail.com>
2026-04-07 11:31:54 -0400

    2025.6.2 solved

--- 2025/6.py
+++ 2025/6.py
@@ -3,7 +3,10 @@
 
 ## Part 1  
 
+Parse a block of text into columns of numbers where either addition or multiplication is performed.
+
 ## Part 2  
+Numbers are now said to be written vertically.  Re-read from the text block and operations redone.
 
 """
 
@@ -11,103 +14,78 @@
 import sys
 
 infile = sys.argv[1] if len(sys.argv) >= 2 else "./inputs/6.in2"
-input = open(infile).read().strip().split("\n")
 
-vals, ops = input[:-1], input[-1].split()
-# print(ops)
-
-vals = [list(map(int, v.split())) for v in vals]
-# print(vals)
-
-num_vals = len(vals)
-num_ops = len(ops)
-
-p1 = 0
-for i in range(num_ops):
-    if ops[i] == "+":
-        temp = 0
-        for ii in range(num_vals):
-            temp += vals[ii][i]
-
-    elif ops[i] == "*":
-        temp = 1
-        for ii in range(num_vals):
-            temp *= vals[ii][i]
-
-    else:
-        raise ValueError(f"Unexpected operation:  {ops[i]}")
-
-    p1 += temp
-
-
-print(f"{'Solution to Part 1:':<20} {p1}")
-
+input = open(infile).read()
 print(input)
 
-
-## Part Two
-v2 = input[:-1]
-o2 = input[-1]
-
-print("Check that all inputs are the same length:")
-for v in v2:
-    print(len(v))
-
-n_rows = len(v2)
-n_cols = len(v2[0])
+input = input.split("\n")[:-1]
+# print(input)
 
 
-id, p2, last_break = 0, 0, 0
+# Observation:  Looks like operator symbol lines up with left-most column of each block
+ops = input[-1]
 
-while id < n_cols:
-    # Find next break in values
+# Find indices where operator symbols exist
+ids = []
+for i in range(len(ops)):
+    if ops[i] != " ":
+        ids.append(i)
 
-    all_blank = True
-    for r in range(n_rows):
-        if v2[r][id] != " ":
-            all_blank = False
-            break
+# Include index of would-be next entry at list end
+ids.append(len(input[1]) + 1)
 
-    if all_blank or id == n_cols:
+p1_totals = []
+for j in range(len(ids) - 1):
+    op = ops[ids[j]]
 
-        print(f"\nFound all blanks at id:  {id}\n")
+    if op == "+":
+        subtotal = 0
+        for jj in range(len(input) - 1):
+            subtotal += int(input[jj][ids[j] : ids[j + 1]])
 
-        # Determine L2R numbers
-        vals = []
-        for ii in range(id - 1, last_break - 1, -1):
+    elif op == "*":
+        subtotal = 1
+        for jj in range(len(input) - 1):
+            subtotal *= int(input[jj][ids[j] : ids[j + 1]])
+    else:
+        raise ValueError(f"Unexpected operator:  {op}")
 
-            print(f"Examining characters at {ii}")
-            val = ""
-            for r in range(n_rows):
-                print(f"Examining locations {r} where value is: {v2[r][ii]}")
+    p1_totals.append(subtotal)
 
-                print(type(v2[r][ii]))
-                val += v2[r][ii]
-                print(val)
 
-            vals.append(int(val))
+print(f"Subtotals for Part 1:\n{p1_totals}")
 
-        print(f"Finished processing block:\n{vals}")
+p1 = sum(p1_totals)
+print(f"\nSolution to Part 1:  {p1}")
 
-        # Perform computation
-        print(f"\nLast break:  {last_break}")
-        if o2[last_break] == "+":
-            p2 += sum(vals)
 
-        elif o2[last_break] == "*":
-            prod = 1
-            for val in vals:
-                prod *= val
+## Part 2:  Perform computation reading number vertically rather than horizontally
 
-            p2 += prod
+p2_totals = []
+for j in range(len(ids) - 1):
+    op = ops[ids[j]]
+    vals = []
+    for jj in range(ids[j], ids[j + 1] - 1):
+        char = []
+        for r in range(len(input) - 1):
+            # print(f"({r}, {jj}):  {input[r][jj]}")
+            char.append(input[r][jj])
 
-        else:
-            raise ValueError(f"Unexpected value:  {ops[last_break]}")
+        vals.append(int("".join(char)))
 
-        print(f"\n\nCurrent value of p2:  {p2}")
+    if op == "+":
+        p2_totals.append(sum(vals))
+    elif op == "*":
+        temp = 1
+        for v in vals:
+            temp *= v
 
-        last_break = id + 1
+        p2_totals.append(temp)
 
-    id += 1
+    else:
+        raise ValueError("Shite")
 
-print(f"{'Solution to Part 2:':<20} {p2}")
+print(f"\nSubtotals for Part 2:\n{p2_totals}")
+
+p2 = sum(p2_totals)
+print(f"\nSolution to Part 2:  {p2}")
