9d13d7b0020e02b4d68a5847fc6a034d9c2b1cd7
Jon Kracht <jonkracht@gmail.com>
2026-04-15 12:40:10 -0400

    Beginning 2025.7

---
+++ 2025/7.py
@@ -0,0 +1,21 @@
+"""
+# Problem description
+
+## Part 1  
+
+## Part 2  
+
+"""
+
+
+import sys
+
+
+infile = sys.argv[1] if len(sys.argv) >= 2 else "./inputs/7.in"
+
+input = open(infile).read().strip()
+
+print(input)
+
+# print(f"{'Solution to Part 1:':<20} {p1}")
+# print(f"{'Solution to Part 2:':<20} {p2}")
