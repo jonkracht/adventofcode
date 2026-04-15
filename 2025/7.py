4c9a6c8d1859b24553e51a03013b3f9d79e1497c
Jon Kracht <jonkracht@gmail.com>
2026-04-15 13:23:18 -0400

    2025.7.1 solved

--- 2025/7.py
+++ 2025/7.py
@@ -2,8 +2,11 @@
 # Problem description
 
 ## Part 1  
+Traverse a "maze" top-to-bottom.  When an obstacle is encountered, split.  Count how many times a split occurs.
+
 
 ## Part 2  
+Count how many distinct paths can be taken from the top of the maze to the bottom.
 
 """
 
@@ -12,10 +15,36 @@ import sys
 
 
 infile = sys.argv[1] if len(sys.argv) >= 2 else "./inputs/7.in"
+input = open(infile).read().strip().split("\n")
+# print(input)
 
-input = open(infile).read().strip()
+# Find index of 'S' (the starting location in first row)
+start = input[0].index("S")
+print(f"S is found at index:  {start}")
 
-print(input)
+locs = []
+locs.append({start})
 
-# print(f"{'Solution to Part 1:':<20} {p1}")
+splits = 0
+
+for i, row in enumerate(input):
+    if i > 0:
+        temp = set()
+        for l in locs[i - 1]:
+            if row[l] == ".":
+                temp.add(l)
+            elif row[l] == "^":
+                splits += 1
+                temp.add(l - 1)
+                temp.add(l + 1)
+            else:
+                raise ValueError(f"Unexpected value:  {row[l]}")
+
+        locs.append(temp)
+
+print(f"{'Solution to Part 1:':<20} {splits}")
+
+
+## Part 2 solution
+
 # print(f"{'Solution to Part 2:':<20} {p2}")
