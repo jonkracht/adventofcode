60aaeecf53aff5f0aa1519b10ad50d311d904a74
Jon Kracht <jonkracht@gmail.com>
2026-04-20 10:23:15 -0400

    Solved 2025.7.2

--- 2025/7.py
+++ 2025/7.py
@@ -2,7 +2,7 @@
 # Problem description
 
 ## Part 1  
-Traverse a "maze" top-to-bottom.  When an obstacle is encountered, split.  Count how many times a split occurs.
+Traverse a "maze" top-to-bottom.  When an obstacle is encountered, split.  Count how many total times a split occurs over the course oftraveling the maze.
 
 
 ## Part 2  
@@ -13,13 +13,13 @@ Count how many distinct paths can be taken from the top of the maze to the botto
 
 import sys
 
-
 infile = sys.argv[1] if len(sys.argv) >= 2 else "./inputs/7.in"
 input = open(infile).read().strip().split("\n")
 # print(input)
 
-# Find index of 'S' (the starting location in first row)
+# Find index of 'S' character, indicating the start location in the first row of the input
 start = input[0].index("S")
+
 print(f"S is found at index:  {start}")
 
 locs = []
@@ -46,5 +46,44 @@ print(f"{'Solution to Part 1:':<20} {splits}")
 
 
 ## Part 2 solution
+nodes = (
+    {}
+)  # nested dictionary whose keys are rows and values are dictionaries of how many routes pass through the point (row/col)
 
-# print(f"{'Solution to Part 2:':<20} {p2}")
+for i, row in enumerate(input):
+
+    print(f"\n** Processing row {i} **")
+
+    subnodes = {}
+    if i == 0:
+        subnodes[start] = 1
+
+    else:
+        for pt, ct in nodes[i - 1].items():
+            char = row[pt]
+
+            # Determine points to add
+            if char == ".":
+                temp = [(pt, ct)]
+
+            elif char == "^":
+                temp = [(pt - 1, ct), (pt + 1, ct)]
+
+            else:
+                raise ValueError(f"Unexpected value {char}")
+
+            # Add to dictionary
+            for k, v in temp:
+                if k in subnodes.keys():
+                    subnodes[k] += v
+                else:
+                    subnodes[k] = v
+
+    nodes[i] = subnodes
+
+# Print results
+for k, v in nodes.items():
+    print(f"{k}:  {v}")
+
+p2 = sum(nodes[len(input) - 1].values())
+print(f"\n{'Solution to Part 2:':<20} {p2}")
